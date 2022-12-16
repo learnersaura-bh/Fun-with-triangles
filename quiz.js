59c1cc628762937c55270449321cc0f3af1f27cd
learnersaura-bh <119805882+learnersaura-bh@users.noreply.github.com>
2022-12-16 10:01:34 +0530

    docs-final-touch

--- quiz.js
+++ quiz.js
@@ -3,7 +3,7 @@ const quizForm = document.querySelector(".quiz-form");
 const submitAnswerButton = document.querySelector("#submit-button");
 const output = document.querySelector("#output")
 
-const rightAnswers = [ "3", "Right Angle", "Equilateral"];
+const rightAnswers = [ "3", "Right Angle", "Equilateral" , "60°" , "centroid"];
 
 
 function calculateScore(){
