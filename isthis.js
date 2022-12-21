e7308f533f6a81710a5bf64e4668d280b462e20c
learnersaura-bh <119805882+learnersaura-bh@users.noreply.github.com>
2022-12-21 09:52:44 +0530

    fix empty-zero-negative

--- isthis.js
+++ isthis.js
@@ -5,16 +5,19 @@ const checkButton = document.querySelector("#check-btn");
 const showMessage = document.querySelector("#show-message")
 function clickEventHandler(){
     const sumOfAngles = Number(angleOne.value) + Number(angleTwo.value) + Number(angleThree.value);
-    if(angleOne.value && angleTwo.value && angleThree.value){if(sumOfAngles === 180){
+    if(angleOne.value && angleTwo.value && angleThree.value)
+    {if(angleOne.value > 0 && angleTwo.value >0 && angleThree.value>0){
+        if(sumOfAngles === 180){
 showMessage.innerText = "These angles form a triangle"
         }
         else{showMessage.innerText = " The angles don't form a triangle"}
-        
     }
+    else{ showMessage.innerText = "The angles must be bigger than 0"}
+}
     else{
 showMessage.innerText = "*Please Enter all fields"
     }
-    console.log(sumOfAngles);
+    // console.log(sumOfAngles);
 }
 
 
